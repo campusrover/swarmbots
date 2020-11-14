9fa92a5e97eba6f0be238dee99d855404c2cbf10
Kelly Duan <kduan@brandeis.edu>
2020-11-14 01:44:20 +0000

    tf

--- src/tf_broadcaster.py
+++ src/tf_broadcaster.py
@@ -11,15 +11,27 @@ from nav_msgs.msg import Odometry
 
 def handle_robot_pose(msg, robot_name):
     br = tf2_ros.TransformBroadcaster()
+    tfBuffer = tf2_ros.Buffer()
+    tfListener = tf2_ros.TransformListener(tfBuffer)
     t = geometry_msgs.msg.TransformStamped()
 
     t.header.stamp = rospy.Time.now()
     t.header.frame_id = "world"
-    t.child_frame_id = robot_name + '/odom'
+    t.child_frame_id = robot_name + '/map'
     t.transform.translation = msg.pose.pose.position
     t.transform.rotation = msg.pose.pose.orientation
 
+    try:
+        # tf prefix
+        trans = tfBuffer.lookup_transform(robot_name + '/odom', robot_name + '/map', rospy.Time())
+        print robot_name, "found map to odom tf"
+    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+        t.child_frame_id = robot_name + '/odom'
+        print e
+    
     br.sendTransform(t)
+
+
 
 rospy.init_node('tf_broadcaster')
 robot_name = rospy.get_namespace()[1:-1]
