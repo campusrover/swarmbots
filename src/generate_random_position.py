12345d36b505ca95191c5727d40a38f7d75818c6
Kelly Duan <kduan@brandeis.edu>
2020-12-11 23:13:29 -0500

    Delete generate_random_position.py

--- src/generate_random_position.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import random, rospy, roslib
-
-rospy.init_node("generate_random_position")
-
-pos_range = float(rospy.get_param('pos_range', 3))
-print(random.uniform(-pos_range / 2, pos_range / 2))
